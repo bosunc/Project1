1e3d0ed68ecfbd8b5285023442ae6b020ed3085b
SKS_hanryang <bosun1985@gmail.com>
2026-05-12 15:30:25 +0900

    선언과 대입.

--- Project1/FileName.cpp
+++ Project1/FileName.cpp
@@ -3,7 +3,20 @@
 using namespace std;
 
 int main() {
-	cout << "hello world" << endl;
+	//cout << "hello world" << endl;
+	//return 0;
+	int a; //선언
+	a = 7; //대입
+	int b = 3; //초기화
+	a = 5;
+	b = 10;
+	// 변수는 사용되기 전에 정의되어야한다.
+	cout << a << b << endl;
+	/*
+	1. 변수는 숫자로 시작할 수 없다.
+	2. C++에서 사용하고 있는 키워드를 변수에 사용할 수 없다. return 같은 애들
+	3. while space를 사용할 수 없다.
+	*/
 	return 0;
 
 }
