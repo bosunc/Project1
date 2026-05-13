303fb66e4215068d0d1f55c45bbf51029386d272
SKS_hanryang <bosun1985@gmail.com>
2026-05-13 17:43:37 +0900

    circle

--- Project1/FileName.cpp
+++ Project1/FileName.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
-
+#define PIE 3.141592
 using namespace std;
 
 int main() {
-	// char : 작은 문자형(아스키코드)
-	int a = 77;
-	char b = a;
-	cout << b << endl;
+	//원의 반지름 구하기
+	//반지름 * 반지름 * PIE
+	int r = 5;
+	float s;
+	s = r * r * PIE;
+	cout << "circle  " << s << "." << endl;
 	return 0;
 
 }
