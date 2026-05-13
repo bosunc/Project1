ff975ee430245037db02679bbbdbda2621c8b4f7
SKS_hanryang <bosun1985@gmail.com>
2026-05-13 17:56:15 +0900

    name size

--- Project1/FileName.cpp
+++ Project1/FileName.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
-#define PIE 3.141592
+#include <cstring>
+
 using namespace std;
 
 int main() {
-	//array
-	short month[12] = { 1, 2, 3, 4 };
-	cout << month[0] << endl;
-	cout << month[7] << endl;
+	//사용자 입력
+	const int Size = 15;
+	char name1[Size];
+	char name2[Size] = "C++Programming";
+
+	cout << "hi how are you? I'm " << name2;
+	cout << "what's your name?\n";
+
+	cin.get(name1, Size);
+	cout << "yes" << name1 << "mr";
+	cout << strlen(name1) << "number\n";
+	cout << sizeof(name1) << "byte size array\n";
+	cout << "your name is " << name1[0] << "is started\n";
+	name2[3] = '\n';
+	cout << "my names first 3charachter is:" << endl;
+	cout << name2 << endl;
 	return 0;
+
 
 
 }
